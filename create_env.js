795a109df1590751cba1252d997be8165428ab7e
Ebakoba <kaarelallemann@gmail.com>
2019-05-26 20:16:26 +0300

    add password and username creation

--- create_env.js
+++ create_env.js
@@ -1,7 +1,31 @@
 const fs = require('fs');
 const crypto = require('crypto');
- 
-fs.writeFile(
-    '.env',
-    'COOKIE_SECRET=' + crypto.randomBytes(40).toString('hex') + '\n'
-);
+const bcrypt = require('bcrypt');
+const prompt = require('prompt');
+
+prompt.start();
+
+
+prompt.get({
+    properties: {
+      username: {
+        pattern: /^[a-zA-Z\s\-]+$/,
+        message: 'Username must be only letters, spaces, or dashes',
+        required: true
+      },
+      password: {
+        hidden: true
+      }
+    }
+  }, function (err, result) {
+    if (err) console.error('Failed to create a .env file');
+
+    const password = bcrypt.hashSync(result.password, 10)
+    
+    fs.writeFile(
+        '.env',
+        'COOKIE_SECRET=' + crypto.randomBytes(40).toString('hex') + '\n' +
+        'USERNAME=' + result.username + '\n' +
+        'PASSWORD=' + password + '\n'
+    );
+});
